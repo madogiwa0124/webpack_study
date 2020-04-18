742943e99424e6ced66deb0e573454e75c800a93
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2020-04-18 14:14:28 +0900

    webpack.config.jsの中でmodeを指定

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  // ビルド時のモード
+  // development: ビルド時間が短くソースマップに対応しているが容量の圧縮がかからない。
+  // production: 本番用build、コメント削除等の圧縮がかかり容量が少ない(デフォルト)
+  mode: "development",
   // jsのエントリーポイント
   // デフォルトはsrc/index.js
   entry: './src/index.js',
